2dcfc383cae23bfd367b14f4ae7db5a68c8ae18a
samt <sam.tlidi@hotmail.co.uk>
2021-03-26 22:04:03 +0000

    done

---
+++ game.rb
@@ -0,0 +1,69 @@
+require "yaml"
+require_relative "board"
+
+  LAYOUTS = {
+    small: { grid_size: 9, num_bombs: 10 }
+  }.freeze 
+class MinesweeperGame
+
+  def initialize(size)
+    layout = LAYOUTS[size]
+    @board = Board.new(layout[:grid_size], layout[:num_bombs])
+
+  end
+
+  def play
+    until @board.won? || @board.lost?
+      puts @board.render
+  
+      action, pos = get_move
+      perform_move(action, pos)
+    end
+    
+    if board.won?
+      puts "You win!"
+    elsif board.lost?
+      puts "**Bomb hit!"
+      puts @board.reveal
+    end
+  end
+
+  private
+
+  def get_move
+    action_type, row_s, col_s = gets.chomp.split(",")
+
+    [action_type, [row_s.to_i, col_s.to_i]]
+  end
+
+  def perform_move(action_type, pos)
+    tile = @board[pos]
+
+    case action_type
+    when "f"
+      tile.toggle_flag
+    when "e"
+      tile.explore
+    when "s"
+      save
+    end
+  end
+
+  def save
+    puts "Enter a filename to save at:"
+    filename = gets.chomp
+
+    File.write(filename, YAML.dump(self)) 
+  end
+end
+
+if $PROGRAM_NAME == __FILE__
+
+  case ARGV.count
+  when 0
+    MinesweeperGame.new(:small).play
+  when 1
+    # resume game, using first argument
+    YAML.load_file(ARGV.shift).play
+  end
+end
